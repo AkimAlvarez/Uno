e90fd6d5fbab1e348d74e31d3c4a4c1a0a6c50c6
Akim <akimalvarez@gmail.com>
2026-06-10 11:01:43 -0300

    implementação da lógica de randomizar as cartas e as jogadas validas

--- dealer.v
+++ dealer.v
@@ -1,12 +1,48 @@
-module tigrinho(input clk, draw, rst
-					 output reg draw_action);
-			
-		reg current_state, next_state;
-		parameter n_cavar = 1`b0,
-					 cavar = 1`b1;
+module tigrinho(input clk, draw, rst,
+				output reg draw_action.
+				output reg [6:0] carta_sorteada);
+
+
+
+		reg[126:0] cartas_disponiveis, cartas_usadas;
+		reg[6:0] contador;	
+
+		reg[1:0] current_state, next_state;
+		parameter 	n_cavar = 2'b00,
+					cavar = 2'b01,
+					esperar_soltar = 2'b10;
 					 
-					 
-		always @ (posedge clk or posedge rst) 
+		always @(posedge clk)
+			begin
+				if(rst) 
+					contador <= 7'd0;
+				else if(contador == 7'd126)
+					contador <=7'd0;
+				else
+					contador <= contador + 7'd1;
+			end
+		
+		always @(posedge clk)
+			begin
+				if (rst) begin
+					current_state <= n_cavar;
+					cartas_usadas <= 127'd0;
+				end
+				else begin
+					current_state <= next_state;
+
+					if (current_state == cavar && cartas_usadas[contador] == 1'b0) begin
+						cartas_usadas[contador] <= 1'b1;
+						carta_sorteada <= contador;
+					end
+				end
+			end
+
+
+
+
+		// FSM para rodar o botão Draw
+		always @ (posedge clk) 
 			begin
 				if(rst)
 					current_state <= n_cavar;
@@ -23,11 +59,17 @@ module tigrinho(input clk, draw, rst
 								else
 									next_state = n_cavar;
 					
-					cavar: if(!draw)
-								next_state = cavar;
-							 
-							 else
-								next_state = n_cavar;
+					cavar: begin
+						if (cartas_usadas[contador] == 1'b0)
+							next_state = esperar_soltar;
+						else
+							next_state = cavar;
+						end
+					esperar_soltar:
+						if(!draw)
+							next_state = esperar_soltar;
+						else
+							next_state = n_cavar;
 								
 					default:next_state = n_cavar;
 				endcase
@@ -36,9 +78,10 @@ module tigrinho(input clk, draw, rst
 		always @(*)
 			begin
 				case(current_state)
-					n_cavar: draw_action = 1`b0;
-					cavar: draw_action = 1`b1;
-					default: draw_action = 1`b0;
+					n_cavar: draw_action = 1'b0;
+					cavar: draw_action = 1'b0;
+					esperar_soltar: draw_action = 1'b1;
+					default: draw_action = 1'b0;
 				endcase
 			end
 
